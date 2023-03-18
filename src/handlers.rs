4d6ccec3da268de757b026e0d2e3aea5eae5277c
carlobortolan <carlobortolan@gmail.com>
2023-03-18 01:20:57 +0100

    Updated basic endpoint structure

--- src/handlers.rs
+++ src/handlers.rs
@@ -13,51 +13,33 @@ pub async fn hello() -> HttpResponse {
 
 // Find out if you're authenticated and authorized to access FG-API
 pub async fn basic_auth_handler(req: HttpRequest) -> impl Responder {
-    let headers = req.headers();
+    let auth_header = match req.headers().get("Authorization") {
+        Some(header) => header,
+        None => return HttpResponse::Unauthorized().body("Unauthorized")
+    };
 
-    if let Some(auth_header) = headers.get("Authorization") {
-        let auth_header_str = auth_header.to_str().unwrap_or_default();
-        if auth_header_str.starts_with("Basic ") {
-            let encoded_credentials = auth_header_str.replace("Basic ", "");
-            let decoded_credentials = decode(&encoded_credentials).unwrap_or_default();
-            let credentials = String::from_utf8(decoded_credentials).unwrap_or_default();
+    let auth_header_str = match auth_header.to_str() {
+        Ok(auth_str) => auth_str,
+        Err(_) => return HttpResponse::Unauthorized().body("Unauthorized")
+    };
 
-            if credentials == format!("{}:{}", env::var("API_USER").unwrap_or_default(), env::var("API_PASSWORD").unwrap_or_default()) {
-                HttpResponse::Ok().body("Authenticated")
-            } else {
-                HttpResponse::Unauthorized().body("Unauthorized")
-            }
-        } else {
-            HttpResponse::Unauthorized().body("Unauthorized")
-        }
+    let encoded_credentials = auth_header_str.replace("Basic ", "");
+    let decoded_credentials = match decode(&encoded_credentials) {
+        Ok(credentials) => credentials,
+        Err(_) => return HttpResponse::Unauthorized().body("Unauthorized")
+    };
+
+    let credentials = match String::from_utf8(decoded_credentials) {
+        Ok(credentials) => credentials,
+        Err(_) => return HttpResponse::Unauthorized().body("Unauthorized")
+    };
+
+    if credentials == format!("{}:{}", env::var("API_USER").unwrap_or_default(), env::var("API_PASSWORD").unwrap_or_default()) {
+        HttpResponse::Ok().body("Authenticated")
     } else {
         HttpResponse::Unauthorized().body("Unauthorized")
     }
 }
-
-// // TODO: update: request body list of jobs and not single job
-// #[post("/feed")]
-// pub async fn load_feed(slice: web::Json<Job>) -> impl Responder {
-//     println!("STARTED FEED");
-//     let json_str = serde_json::to_string(&slice).unwrap();
-//     println!("slice = {}", json_str);
-//
-//     let _jobs = match parse_jobs_from_json(&json_str) {
-//         Ok(jobs) => jobs,
-//         Err(e) => {
-//             eprintln!("Error parsing jobs from feed JSON: {}", e);
-//             return HttpResponse::BadRequest().finish();
-//         }
-//     };
-//
-//     // TODO: call Ranker
-//     // TODO: call Logger
-//     // TODO: respond with result
-//
-//     HttpResponse::Created().finish()
-// }
-
-
 
 // TODO: update: request body list of jobs and not single job
 #[post("/feed")]
@@ -80,12 +62,15 @@ pub async fn load_feed(slice: web::Json<Job>, req: HttpRequest) -> impl Responde
     }
 }
 
+// Parse request body and rank jobs
 fn process_feed_request(slice: Job) -> Result<(), String> {
     println!("STARTED FEED");
 
+    // Convert request body to string
     let json_str = serde_json::to_string(&slice).map_err(|e| e.to_string())?;
     println!("PARSED JSON = {}", json_str);
 
+    // Parse string to job
     let jobs = match parse_jobs_from_json(&json_str) {
         Ok(jobs) => jobs,
         Err(e) => return Err(format!("Error parsing jobs from feed JSON: {}", e)),
@@ -98,6 +83,7 @@ fn process_feed_request(slice: Job) -> Result<(), String> {
     Ok(())
 }
 
+// Check if user is authenticated and authorized to access FG-API
 pub fn is_authorized(req: &HttpRequest) -> bool {
     if let Some(auth_header) = req.headers().get("Authorization") {
         if let Ok(auth_str) = auth_header.to_str() {
