052d9cf880e8e28d08a0cf66d946df285a725dc7
carlobortolan <carlobortolan@gmail.com>
2023-03-18 01:31:36 +0100

    Updated project structure

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////MAIN////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 use std::env;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
 
 mod handlers;
 mod auth;
-mod job_slicer;
 mod models;
+mod ranker;
+mod logger;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -14,8 +19,8 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(handlers::hello)
             .service(handlers::load_feed)
-            // .route("/", web::get().to(hello_world))
-            .route("/basic-auth", web::get().to(handlers::basic_auth_handler))
+            .service(handlers::basic_auth)
+            // .route("/auth", web::get().to(handlers::basic_auth))
     })
     .bind(env::var("ADDRESS").unwrap_or_else(|_| "127.0.0.1:8080".to_string()))?
     .run()
