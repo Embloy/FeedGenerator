cb58f72de1f15332399ab89ab7ac4f6a6195b613
Carlo Bortolan <carlo.bortolan@tum.de>
2023-03-21 15:16:14 +0100

    Updated ranker.rs to work with no preferences

--- src/ranker.rs
+++ src/ranker.rs
@@ -5,23 +5,35 @@
 use crate::{meta, t_score};
 use crate::models::{Job, UserPreferences};
 
-fn sort_jobs_by_relevance(jobs: &mut Vec<Job>, preferences: &mut UserPreferences) -> Vec<Job> {
-    t_score::calc_x_ranking(preferences);
-    jobs.sort_by(|a, b| {
-        let a_score = job_relevance_score(a, preferences);
-        let b_score = job_relevance_score(b, preferences);
-        b_score.partial_cmp(&a_score).unwrap()
-    });
+fn sort_jobs_by_relevance(jobs: &mut Vec<Job>, preferences: &mut Option<UserPreferences>) -> Vec<Job> {
+    if preferences.is_some() {
+        t_score::calc_x_ranking(&mut preferences.clone().unwrap());
+        jobs.sort_by(|a, b| {
+            let a_score = job_relevance_score(a, &preferences.clone().unwrap());
+            let b_score = job_relevance_score(b, &preferences.clone().unwrap());
+            b_score.partial_cmp(&a_score).unwrap()
+        });
+    } else {
+        jobs.sort_by(|a, b| {
+            let a_score = job_relevance_score_no_pref(a);
+            let b_score = job_relevance_score_no_pref(b);
+            b_score.partial_cmp(&a_score).unwrap()
+        });
+    }
     jobs.clone()
 }
 
 fn job_relevance_score(job: &Job, preferences: &UserPreferences) -> f64 {
     let x = 0.5;
     meta::calc_score(job, preferences) * x + t_score::calc_score(job, preferences) * (1.0 - x)
-    //meta::calc_score(job, preferences) * x + 0.0 * (1.0 - x)
 }
 
-pub fn generate_job_feed(jobs: Vec<Job>, preferences: Option<UserPreferences>) -> Vec<Job> {
-    sort_jobs_by_relevance(&mut jobs.clone(), &mut preferences.unwrap())
+fn job_relevance_score_no_pref(job: &Job) -> f64 {
+    println!("ALERT! [NO PREFERENCES FOUND]");
+    meta::calc_score_no_pref(job)
+}
+
+pub fn generate_job_feed(jobs: Vec<Job>, mut preferences: Option<UserPreferences>) -> Vec<Job> {
+    sort_jobs_by_relevance(&mut jobs.clone(), &mut preferences)
     // TODO: Shadowing ...
 }
