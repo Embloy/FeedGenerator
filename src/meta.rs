332130168c3c14c1ba4edf90004ff4d1a0d40f2d
github4touchdouble <touchedouble@gmx.de>
2023-03-24 01:37:56 +0100

    updated ranking algorithm

--- src/meta.rs
+++ src/meta.rs
@@ -8,10 +8,9 @@
 // SP: [0,1] * 0.1
 // => MIN META-SCORE = -0.4
 // => MAX META-SCORE = 3
-
 use std::any::Any;
-use chrono::{DateTime, Utc};
-
+use actix_web::cookie::time::macros::datetime;
+use chrono::{DateTime, NaiveDateTime, ParseError, TimeZone, Utc};
 use crate::models::{Job, UserPreferences};
 
 const ER_WF: f64 = 0.2;
@@ -20,6 +19,7 @@ const SR_WF: f64 = 0.2;
 const SP_WF: f64 = 0.1;
 
 pub fn calc_score(job: &Job, pref: &UserPreferences) -> f64 {
+    println!("for job id {} employer_score is {} trend_factor is {} salaryrange is {} spontaneity is {}", job.job_id, employer_rating(job), trend_factor(job), salary_range(job, pref), spontaneity(job, pref));
     employer_rating(job) * ER_WF + trend_factor(job) * TF_WF + salary_range(job, pref) * SR_WF + spontaneity(job, pref) * SP_WF
 }
 
@@ -37,28 +37,21 @@ fn employer_rating(job: &Job) -> f64 {
 }
 
 fn trend_factor(job: &Job) -> f64 {
-
     let applications = job.applications_count as f64;
-    let view_weight:f64;
+    let view_weight: f64;
     let views = job.view_count as f64;
     if views <= 100.0 {
         view_weight = 0.3;
-    }else if views > 100.0 && views <= 500.0 {
+    } else if views > 100.0 && views <= 500.0 {
         view_weight = 1.0;
-    }else if views != 0.0 {
-        view_weight = 1.5/(1.00001-applications/views);
-    }else {
+    } else if views != 0.0 {
+        view_weight = 1.5 / (1.00001 - applications / views);
+    } else {
         view_weight = 0.0;
     }
     let score = (job.applications_count as f64 + 1.0).log10() / (views + 1.0).log10();
-    println!("For {} the views {} and applications {} add up to the non weighted score {} or weighted score {}", job.job_id, views, applications, score, score * view_weight);
+    //println!("For {} the views {} and applications {} add up to the non weighted score {} or weighted score {}", job.job_id, views, applications, score, score * view_weight);
     score * view_weight
-    /*
-      if job.applications_count > 0 && job.view_count > 0 {
-          (job.applications_count * 10 / job.view_count) as f64
-      } else { 0.0 }
-    */
-
 }
 
 fn salary_range(job: &Job, pref: &UserPreferences) -> f64 {
@@ -75,16 +68,34 @@ fn salary_range(job: &Job, pref: &UserPreferences) -> f64 {
 }
 
 fn spontaneity(job: &Job, pref: &UserPreferences) -> f64 {
-    // Get spontaneity preference
+    // Get spontaneity preference => x value of peak
     let p: f64 = pref.spontaneity.unwrap_or_default();
     // Parse start_slot
-    let start_slot = DateTime::<Utc>::from_utc(
-        chrono::DateTime::parse_from_rfc3339(&job.start_slot)
-            .unwrap()
-            .naive_utc(),
-        Utc,
-    );
+    let start_slot = Utc
+        .datetime_from_str(&job.start_slot.trim(), "%Y-%m-%dT%H:%M:%S%.3fZ")
+        .expect("Failed to parse datetime string");
+
+    let start_slot_sec = (start_slot.signed_duration_since(Utc::now()).num_seconds()) as f64;
+    let result = spontaneity_map(start_slot_sec, p);
     // Return ratio of preference to time-delta
-    p / (start_slot.signed_duration_since(Utc::now()).num_days() as f64)
+    //let result = p / (start_slot.signed_duration_since(Utc::now()).num_seconds()) as f64;
+    result
 }
+
+fn spontaneity_map(a: f64, b: f64) -> f64 {
+    let steepness = 0.15; // steepness factor; should be adapted if score is to impactful
+    let distance = (a - b).abs();
+    if distance > 86400.0 {
+        return 0.0;
+    }
+    let fa =
+        2.5 - steepness * distance.ln();
+
+    if distance < 1000.0 {
+        return 2.5;
+    }
+    return fa.max(0.0).min(2.5);
+}
+
+
 
--- src/ranker.rs
+++ src/ranker.rs
@@ -7,7 +7,7 @@ use crate::models::{Job, UserPreferences};
 
 fn sort_jobs_by_relevance(jobs: &mut Vec<Job>, preferences: &mut Option<UserPreferences>) -> Vec<Job> {
     if preferences.is_some() {
-        t_score::calc_x_ranking(&mut preferences.clone().unwrap());
+        t_score::calc_x_ranking(&mut preferences.as_mut().unwrap());
         jobs.sort_by(|a, b| {
             let a_score = job_relevance_score(a, &preferences.clone().unwrap());
             let b_score = job_relevance_score(b, &preferences.clone().unwrap());
@@ -25,6 +25,7 @@ fn sort_jobs_by_relevance(jobs: &mut Vec<Job>, preferences: &mut Option<UserPref
 
 fn job_relevance_score(job: &Job, preferences: &UserPreferences) -> f64 {
     let x = 0.3;
+    println!("calc meta score {} calc t_score {}", meta::calc_score(job, preferences), t_score::calc_score(job, preferences));
     meta::calc_score(job, preferences) * x + t_score::calc_score(job, preferences) * (1.0 - x)
 }
 
