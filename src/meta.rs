13383562e01ae63851ddf2d1228b6fc157962152
github4touchdouble <touchedouble@gmx.de>
2023-03-29 14:15:39 +0200

    bugfix

--- src/meta.rs
+++ src/meta.rs
@@ -19,7 +19,7 @@ const SR_WF: f64 = 0.2;
 const SP_WF: f64 = 0.1;
 
 pub fn calc_score(job: &Job, pref: &UserPreferences) -> f64 {
-    println!("for job id {} employer_score is {} trend_factor is {} salaryrange is {} spontaneity is {}", job.job_id, employer_rating(job), trend_factor(job), salary_range_B(job, pref), spontaneity(job, pref));
+    //println!("for job id {} employer_score is {} trend_factor is {} salaryrange is {} spontaneity is {}", job.job_id, employer_rating(job), trend_factor(job), salary_range_B(job, pref), spontaneity(job, pref));
     employer_rating(job) * ER_WF + trend_factor(job) * TF_WF + salary_range_B(job, pref) * SR_WF + spontaneity(job, pref) * SP_WF
 }
 
